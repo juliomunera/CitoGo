0575123fbb4138b09f0b129e595d6835ba36002a
juliomunera <julx77@gmail.com>
2020-05-16 22:07:28 -0500

    create new slide pages

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -10,20 +10,7 @@ export class FolderPage implements OnInit {
   public folder: string;
 
   constructor(private activatedRoute: ActivatedRoute) { 
-    let systemDark = window.matchMedia("(prefers-color-scheme: light)");
-    systemDark.addListener(this.colorTest);
-
-      document.body.setAttribute('data-theme', 'light');
   }
-
-  colorTest(systemInitiatedDark) {
-    if (systemInitiatedDark.matches) {
-      document.body.setAttribute('data-theme', 'dark');		
-    } else {
-      document.body.setAttribute('data-theme', 'light');
-    }
-  }
-  
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
